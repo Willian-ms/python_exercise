932f7a6a2bc579a5ef2baed9bcfbaaf5506c5afa
willian-ms <willian.ms@icloud.com>
2022-03-14 16:14:34 -0300

    comentario

--- ex1.py
+++ ex1.py
@@ -1,3 +1,6 @@
+#	Escreva um programa que receba 2 valores do tipo inteiro x e y, e calcule e imprima o valor de z:
+#	z=(x^2+y^2)/(x-y)
+
 x = int(input('Digite x: '))
 y = int(input("Digite y: "))
 
